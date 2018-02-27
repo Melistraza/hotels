0dab7c254cd3fdadeff47ef3a4cebf9e551eb857
Melistraza <melistraza@gmail.com>
2018-02-27 13:26:06 +0200

    update docker-entrypoint-api.sh

--- docker-entrypoint-api.sh
+++ docker-entrypoint-api.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 ./manage.py collectstatic --noinput
-./manage.py loaddata apps/rooms/fixtures/test_user.json
 
 until python manage.py migrate; do
   >&2 echo "Postgres database is unavailable - sleeping"
   sleep 1
 done
+
+./manage.py loaddata apps/rooms/fixtures/test_user.json
 
 gunicorn config.wsgi:application --workers 2 --bind :8000
